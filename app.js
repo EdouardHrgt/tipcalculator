c457f41a65233f7508fb235c960a3fa461ee1dbf
Edouard Herrengt <herrengt.edouard@gmail.com>
2022-01-15 15:15:52 +0100

    code fonctionnel, manque customisation de la couleur du % selectionné

--- app.js
+++ app.js
@@ -4,52 +4,75 @@ const billInput = document.querySelector("#bill-input");
 let billInputValue = 0.0;
 
 billInput.addEventListener("change", () => {
+  const ErrMsg = document.querySelector("#bill-error");
   billInputValue = parseFloat(billInput.value);
+  if (isNaN(billInputValue)) {
+    ErrMsg.innerHTML = "Please use only numbers";
+  } else if (billInputValue > 10000) {
+    ErrMsg.innerHTML = "can't be more than 10.000$";
+  } else {
+    ErrMsg.innerHTML = "";
+    totals();
+  }
 });
 
 // Choix du pourboire
 const tipAmount = document.querySelectorAll(".input-percent");
-const tipsList = [0.05, 0.1, 0.15, 0.25, 0.5];
-let tipChoice = 0;
+const tipsList = [1.05, 1.1, 1.15, 1.25, 1.5];
+let tipChoice = 1.05;
+const percentColor = document.querySelectorAll(".label-percent");
 
 tipAmount.forEach(function (tip, i) {
   tip.addEventListener("click", () => {
     tipChoice = tipsList[i];
+    if (percentColor[i].classList.contains("active")) {
+      percentColor[i].classList.remove("active");
+    } else {
+      percentColor[i].classList.add("active");
+    }
+
+    totals();
   });
 });
 
 const customTip = document.querySelector("#custom-input");
 
 customTip.addEventListener("change", () => {
-  tipChoice = parseFloat(customTip.value / 100);
+  tipChoice = parseFloat(`1.${customTip.value}`);
+  totals();
 });
 
 // Choix nombre de participants
 const peopleInput = document.querySelector("#people-input");
-const errMsg = document.querySelector(".error-msg");
-let peopleNumber = 0;
+let peopleNumber = 1;
 
 peopleInput.addEventListener("change", () => {
+  const ErrMsg = document.querySelector("#people-error");
   if (peopleInput.value <= 0) {
-    errMsg.innerHTML = "Can't be zero";
+    ErrMsg.innerHTML = "Can't be zero";
   } else {
     peopleNumber = parseFloat(peopleInput.value);
-    errMsg.innerHTML = "";
+    ErrMsg.innerHTML = "";
+    totals();
   }
 });
 
 //Affichage des résultats
-const tipTotalDisplay = document.querySelector(".tip-result");
-const tipPerPeopleDisplay = document.querySelector(".total-result");
+const tipDisplay = document.querySelector(".tip-result");
+const totalDisplay = document.querySelector(".total-result");
 
-function Totals() {
-  let total = Math.round(billInputValue + (tipChoice * 100) / peopleNumber);
-  console.log(total);
-  tipPerPeopleDisplay.innerHTML = `$ ${total}`;
+function totals() {
+  let total = Math.round((billInputValue * tipChoice) / peopleNumber);
+  totalDisplay.innerHTML = `$ ${total}`;
+
+  let tipPerPerson = Math.round(
+    [billInputValue * tipChoice - billInputValue] / peopleNumber
+  );
+  tipDisplay.innerHTML = `$ ${tipPerPerson}`;
 }
+
 // Bouton reset
 const resetBtn = document.querySelector(".reset-btn");
 resetBtn.addEventListener("click", () => {
-  //location.reload();
-  Totals();
+  location.reload();
 });
